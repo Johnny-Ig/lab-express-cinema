a4669229dbbddcca25eba3986d6a110282737226
Jonatan Iglesias <jonathan-18719@outlook.es>
2023-05-07 23:49:26 +0200

    done

---
+++ models/Movie.models.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const movieSchema = new mongoose.Schema({
+  title: String,
+  director: String,
+  stars: [String],
+  image: String,
+  description: String,
+  showtimes: [String],
+});
+
+const Movies = mongoose.model("movies", movieSchema)
+module.exports = Movies
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,39 @@
 const express = require('express');
+const Movies = require('../models/Movie.models');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req,res, next)=>{
+    Movies.find()
+    .select({title: 1, image: 1})
+    .then((response)=>{
+        // console.log(response);
+        res.render("movies.hbs",{
+            allMovies: response
+        });
+    })
+    .catch((error)=>{
+        next(error)
+    })
+})
+
+router.get("/movies/:id", async (req,res, next)=>{
+    try{
+        const response = await Movies.findById(req.params.id);
+        console.log(response);
+        res.render("movie-details.hbs", {
+           infoMovie: response 
+        });
+    }
+    catch (error) {
+        next(error);
+      }
+
+    
+})
+
+
 
 module.exports = router;
